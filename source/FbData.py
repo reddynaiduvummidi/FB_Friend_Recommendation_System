9272a21d4b1fef65d08444ca464438e1f33c6db6
saurabh singh <singh.saurab@husky.neu.edu>
2017-10-09 20:01:20 -0400

    code now reading FB data for user and their friends and their friends. Thats 2 level

--- source/FbData.py
+++ source/FbData.py
@@ -1,6 +1,7 @@
 import os
 import pprint
 import requests
+import pickle
 
 class FbData(object):
     """
@@ -11,20 +12,62 @@ class FbData(object):
         """
         Constructor for FbData class
         """
-        FBACCESSTOKEN=""
-        # self.accessToken =os.environ['FBACCESSTOKEN']
-        self.accessToken = FBACCESSTOKEN
-        print (self.accessToken)
+        self.accessToken =os.environ['FBACCESSTOKEN']
 
     def reqData(self, req):
         """
         Method requests data from fb Graph API explorer.
-        :return:
+        :return: data fetched from Graph API
         """
         response = requests.get("https://graph.facebook.com/v2.10/" + req,
                      {'access_token':self.accessToken})
         print (response)
-        print (response.json())
+        return response.json()
+
+    def CollectData(self, queryFilename):
+        """
+        Method is used to get data from facebook
+        Query used in this method is saved in query.txt file in ../config
+        folder
+
+        Function extracts 18 different features for a particular user and
+        thier friends and their freinds as well
+
+        Wooh! Thats 2 level nesting
+
+        Function writes the data fetched from Graph API into file with
+        filename = userID
+
+        :return: None
+        """
+        with open(queryFilename,"r") as f:
+            query = f.readline()
+            data = self.reqData(query)
+            self.writeData(data, "../output/1.txt")
+
+    def writeData(self, data, filename):
+        """
+        Function writes the JSON formatted data to file with filename =
+        filename
+        :param data: JSON formatted data from Facebook
+        :param filename: filename to be written on the system
+        :return: None
+        """
+        with open(filename, "wb") as fileHandler:
+            pickle.dump(data, fileHandler, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def readData(self, filename):
+        """
+        Method reads the facebook Graph API dump data already stored on the
+        disk in pickle format. The data is in JSON format
+        :param filename : filename for the data stored on the i=disk
+        :return : None
+        """
+        with open(filename, "rb") as fileHandler:
+            data = pickle.load(fileHandler)
+            print (data)
+
+
 
 def main():
     """
@@ -32,12 +75,11 @@ def main():
     calls its download method
     :return: None
     """
-    # requestion total comments and likes per post from SarcasmLOL FB page
-    reqString = "sarcasmLOL?fields=posts{comments.limit(0).summary(true),likes.limit(0).summary(true)}"
-    reqString2 = "me?fields=birthday,about,education"
+    queryFilename = "../config/query.txt"
 
     fb = FbData()
-    fb.reqData(reqString)
+    fb.CollectData(queryFilename)
+    fb.readData("../output/1.txt")
 
 if __name__ == "__main__":
     main()
