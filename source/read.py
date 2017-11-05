34b59167058ed479b603d3e2acb47ad50b02dba3
kz4 <zkcneu@gmail.com>
2017-11-04 20:19:24 -0400

    count the friends that have no friends, which need to be requeried

--- source/read.py
+++ source/read.py
@@ -8,8 +8,30 @@ def read_json_to_object(filename):
 
     # Parse JSON into an object with attributes corresponding to dict keys.
     x = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-    print(x.name)
-    print([x.name for x in x.friends.data])
+    x, i = filter_people_with_no_firends(x)
+    print(x)
+    print(i)
+
+def filter_people_with_no_firends(x):
+    """
+    res is all friends of the original person
+    i is the number of friends that need to requery because they don't have friends
+    """
+    res = []
+    i = 0
+    for friend in x.friends.data:
+        try:
+            if len(friend.friends.data) > 0:
+                res.append(friend)
+        except Exception as e:
+            i += 1
+            pass
+        else:
+            pass
+    return res, i
+
+def is_self(friend, id):
+    return friend.id == id
 
 def main():
     """
